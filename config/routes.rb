59e137c9e51255a0f8e85fde9a7d3aba624b31bf
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-09 17:14:30 -0600

    Add functionality to remove favs from pets show page

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
 
   get "/favorites", to: 'favorites#index'
   patch "/favorites/:pet_id", to: 'favorites#update'
+  delete "/favorites/:pet_id", to: 'favorites#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
